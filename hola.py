b81d1e7a18e381b0a1386c58e7c8bbf37721d383
KyamiV <camivalencia090@gmail.com>
2025-06-22 11:02:01 -0500

    agrego la funcion para saludar en ingles

--- hola.py
+++ hola.py
@@ -2,3 +2,12 @@ def saludar(nombre):
     print("Hola, " + nombre)
 
 saludar("Camila")
+
+def saludarIngles(nombre):
+    print("Hello, " + nombre)
+
+saludarIngles("Camila")
+
+def saludarFrances(nombre):
+    print("Bonjour, " + nombre)
+saludarFrances("Camila")
